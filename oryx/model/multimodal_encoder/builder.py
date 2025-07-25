902a26127145241f9a573a18aff4e2ba27afe3c0
zerowing-ex <55009709+zerowing-ex@users.noreply.github.com>
2025-07-25 20:34:59 +0800

    Update builder.py
    New feat: support local models loading
    ValueError: Unknown vision tower: /models/Oryx-ViT

--- oryx/model/multimodal_encoder/builder.py
+++ oryx/model/multimodal_encoder/builder.py
@@ -8,5 +8,9 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
         print(f"Buiding OryxViTWrapper from {vision_tower}...")
         path = vision_tower.split(":")[1]
         return OryxViTWrapper(vision_tower, path=path, args=vision_tower_cfg, **kwargs)
+    elif "Oryx-ViT" in vision_tower and os.path.exists(vision_tower) and os.path.isabs(vision_tower):
+        print(f"Buiding OryxViTWrapper from {vision_tower}...")
+        path = vision_tower
+        return OryxViTWrapper(vision_tower, path=path, args=vision_tower_cfg, **kwargs)
     else:
         raise ValueError(f'Unknown vision tower: {vision_tower}')
